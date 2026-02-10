cf113fb4dc497a04bfaf06eb20c02873add01ce9
MaxGOUET <maxime.gouet1@gmail.com>
2026-02-10 14:08:10 +0100

    server ok + link mongoDB ok

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const Book = require("./models/Book");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  );
+  next();
+});
+
+app.get("/api/books", (req, res, next) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+module.exports = app;
---
+++ backend/models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  rating: [{ userId: String, grade: Number }],
+  averageRating: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
